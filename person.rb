bc4b766677e9bacecfaf4bf0218aaa5dc045a2c3
Daniel Ochuba <danielochuba78@gmail.com>
2023-08-15 14:24:49 +0100

    modify Person class to use decoration

--- person.rb
+++ person.rb
@@ -1,12 +1,17 @@
-class Person
+require_relative 'capitalize'
+require_relative 'nameable'
+require_relative 'decorator'
+require_relative 'trim'
+
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, parent_permission: true, name: 'Unknown')
+  def initialize(age, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..100)
     @name = name
     @age = age
-    @parent_permission = parent_permission
   end
 
   def can_use_services?
@@ -15,9 +20,22 @@ class Person
     true
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
+person = Person.new(22, name: 'maximilianus')
+puts person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
