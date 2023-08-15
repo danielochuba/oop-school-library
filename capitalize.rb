c25207e8bf79fb8bade5b22118e26a9b533c21d3
Daniel Ochuba <danielochuba78@gmail.com>
2023-08-15 14:18:58 +0100

    add CapitalizeDecorator class

--- capitalize.rb
+++ capitalize.rb
@@ -1,1 +1,11 @@
+require_relative 'decorator'
 
+class CapitalizeDecorator < BaseDecorator
+  def initialize(person)
+    @nameable = person
+  end
+
+  def correct_name
+    @nameable.correct_name.upcase
+  end
+end
