8597c4c2865a53c9152e6638e365335bca25b0bc
Daniel Ochuba <danielochuba78@gmail.com>
2023-08-15 14:45:25 +0100

    fix super omission

--- person.rb
+++ person.rb
@@ -8,6 +8,7 @@ class Person < Nameable
   attr_accessor :name, :age
 
   def initialize(age, name: 'Unknown', parent_permission: true)
+    super
     @id = Random.rand(1..100)
     @name = name
     @age = age
