4747143a392e7b93c78776f01cceae45a318574d
Daniel Ochuba <danielochuba78@gmail.com>
2023-08-18 00:52:10 +0100

    add main file to test

---
+++ main.rb
@@ -0,0 +1,12 @@
+require_relative 'student'
+require_relative 'classroom'
+require_relative 'capitalize'
+require_relative 'nameable'
+require_relative 'decorator'
+require_relative 'trim'
+require_relative 'book'
+require_relative 'rental'
+require_relative 'nameable'
+require_relative 'person'
+
+
