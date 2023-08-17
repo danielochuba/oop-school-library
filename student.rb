00bb57ca66fd1ff84870111c1cf964b263c23723
Daniel Ochuba <danielochuba78@gmail.com>
2023-08-18 00:53:05 +0100

    modify student class to fit association

--- student.rb
+++ student.rb
@@ -1,12 +1,20 @@
+require_relative 'classroom'
 require_relative 'person'
 
 class Student < Person
-  def initialize(classroom, age, name: 'Unknown', parent_permission: false)
-    super(age, parent_permission: parent_permission, name: name)
+  attr_reader :classroom
+
+  def initialize(classroom, age, name = 'Unknown', parent_permission = false)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
     '¯(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
