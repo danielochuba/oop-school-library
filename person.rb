496fd6c260d27abf0c466f487251604b34aa3e21
Daniel Ochuba <danielochuba78@gmail.com>
2023-08-17 19:46:00 +0100

    modify parameters to accept required arguments

--- person.rb
+++ person.rb
@@ -7,7 +7,7 @@ class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     super()
     @id = Random.rand(1..100)
     @name = name
--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,15 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(specialization, age, name: 'Unknown', parent_permission: true)
-    super(age, parent_permission: parent_permission, name: name)
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
+
+  attr_accessor :specialization
 
   def can_use_services?
     true
   end
+
 end
